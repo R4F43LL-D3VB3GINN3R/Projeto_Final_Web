45ef4b337cd671d2cedd84fa856f7ea0d18ffbac
Rafael Albuquerque <139004366+R4F43LL-D3VB3GINN3R@users.noreply.github.com>
2024-04-09 01:59:05 +0100

    Update admins_insert.php

--- admins_insert.php
+++ admins_insert.php
@@ -8,66 +8,65 @@
     <link rel="stylesheet" href="styles/insert_message.css">
 </head>
 <body>
-
-<?php
-
-$hostname = "localhost";
-$database = "db_devsync";
-$username = "root";
-$password = "";
-
-$conn = new mysqli($hostname, $username, $password, $database);
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    $nome_ = $_POST['nome'];
-    $login_ = $_POST['login'];
-    $senha_ = $_POST['senha'];
-
-    $stmt = $conn->prepare("INSERT INTO tab_admin (nome, login, pass) VALUES (?, ?, ?)");
-    $stmt->bind_param('sss', $nome_, $login_, $senha_);
-    $stmt->execute();
-
-    if ($stmt->affected_rows > 0) {
+    <?php
+    //Conexão com a base de dados.
+    include 'conexao.php';
+    //Se o método for post...
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        //Recupera dados do formulário.
+        $nome_ = $_POST['nome'];
+        $login_ = $_POST['login'];
+        $senha_ = $_POST['senha'];
+        //Executa a Query SQL.
+        $stmt = $conn->prepare("INSERT INTO tab_admin (nome, login, pass) VALUES (?, ?, ?)");
+        $stmt->bind_param('sss', $nome_, $login_, $senha_);
+        $stmt->execute();
+        //Se alguma linha for afetada...
+        if ($stmt->affected_rows > 0) {
 
     ?>
-
-          <div id="message_div">
+        <?php // Apresenta caixa de diálogo. ?>
+        <div id="message_div"> 
             <h1>Novo Administrador Cadastrado</h1>
             <button onclick="redirect1()" id="buttonmessage">Ok</button>
-          </div>  
-          <script>
+        </div>  
+        <script>
+            //Função para redirecionar.
             function redirect1() {
 
                 window.location.href = "admins.php";
 
             }
-          </script>
-
+        </script>
+        <?php // Do contrário...?>
     <?php
 
-    } else {
+        } else {
 
     ?>
+        <?php  //Exibe caixa de diálogo. ?>
         <div id="message_div">
             <h1>Não foi possível adicionar o Administrador</h1>
             <button onclick="redirect2()">Ok</button>        
         </div>
+        
         <script>
+            //Função para redirecionar.
             function redirect2() {
 
                 window.location.href = "admins_insert.html";
 
             }
         </script>
+
     <?php
+
+        }
+        //Encerra a conexão.
+        $conn->close();
 
     }
 
-    $conn->close();
-
-}
-
-?>
+    ?>
 </body>
 </html>
