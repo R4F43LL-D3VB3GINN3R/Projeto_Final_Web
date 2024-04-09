2fcf89f32732a654e6d767b0c85863c486710da9
Rafael Albuquerque <139004366+R4F43LL-D3VB3GINN3R@users.noreply.github.com>
2024-04-09 17:57:35 +0100

    Update pedidos_recusados.php

--- pedidos_recusados.php
+++ pedidos_recusados.php
@@ -1,19 +1,15 @@
 <?php
-
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$database = "db_devsync";
-
-$conn = new mysqli($hostname, $username, $password, $database);
-
+//Conexão com a base de dados.
+include 'conexao.php';
+//Método Get para recuperar o nome do cliente.
 $cliente = $_GET['cliente'];
-
+//Excecuta a Query.
+//Remove o pedido da tabela de cliente.
 $stmt = $conn->prepare("DELETE FROM tab_pedido WHERE CLIENTE = ?");
 $stmt->bind_param('s', $cliente);
 $stmt->execute();
-
+//Encerra a conexão.
 $conn->close();
+//Redireciona para o url.
 header("Location: pedidos.php");
-
 ?>
