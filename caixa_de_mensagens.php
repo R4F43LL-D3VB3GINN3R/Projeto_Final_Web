3cd8f618c1d69f268b853a7b6e8fb192db17a77e
Rafael Albuquerque <139004366+R4F43LL-D3VB3GINN3R@users.noreply.github.com>
2024-04-09 02:32:09 +0100

    Update caixa_de_mensagens.php

--- caixa_de_mensagens.php
+++ caixa_de_mensagens.php
@@ -11,48 +11,41 @@
 <body>
     <section class="layout">
         <div class="menu1"><a href="admins.php">Admins</a></div>
-        <div class="body">
-
-            <h2>Mensagens Pendentes</h2>
-
-            <?php
-
-            $hostname = "localhost";
-            $username = "root";
-            $pass = "";
-            $database = "db_devsync";
-
-            $conn = new mysqli($hostname, $username, $pass, $database);
-
-            $sql = "SELECT * FROM tab_contato WHERE RESPOSTA = 'PENDENTE'";
-            $result = $conn->query($sql);
-
-            if ($result->num_rows > 0) {
-
-                $contatos_emails_mensagens = array(); 
-                
-
-                while ($row = $result->fetch_assoc()) {
-                    $contatos_emails_mensagens[$row['EMAIL']] = $row['MENSAGEM'];
-                }
-                
-                    echo '<select name="messages" id="messages">';
-                    foreach ($contatos_emails_mensagens as $email => $mensagem) {
-                        echo '<option value="' . $email . '">' . $email . '</option>';
-                }
-                
-                echo '</select>';
-
-            }
-
-            $conn->close();
-
-            ?>
-
-            <br><br>
-            <textarea name="areamessage" id="areamessage" cols="30" rows="10"></textarea>
-            <button onclick="redirect()" id="Respondido">Respondido</button>
-
+            <div class="body">
+                <h2>Mensagens Pendentes</h2>
+                <?php
+                    //Conexão com base de dados.
+                    include 'conexao.php';
+                    //Executa a Query.
+                    $sql = "SELECT * FROM tab_contato WHERE RESPOSTA = 'PENDENTE'";
+                    //A variável $result é do tipo mysqli_result, que é uma classe que representa o conjunto de resultados de uma consulta SQL.
+                    $result = $conn->query($sql);
+                    //Se o número de linhas em result for maior do que zero...
+                    if ($result->num_rows > 0) {
+                        //Cria um array para receber os emails e mensagens da consulta.
+                        $contatos_emails_mensagens = array(); 
+                        //Enquanto houver dados encontrados...
+                        while ($row = $result->fetch_assoc()) {
+                            //Guarda no array os emails e mensagens encontrados.
+                            //Os emails e mensagens estão relacionados na mesma linha.
+                            $contatos_emails_mensagens[$row['EMAIL']] = $row['MENSAGEM'];
+                        }
+                        //Cria um dropdown.                  
+                        echo '<select name="messages" id="messages">';
+                        //Itera sobre o vetor usando o email como chave, associando à respectiva mensagem.
+                        foreach ($contatos_emails_mensagens as $email => $mensagem) {
+                            //Preenche a dropdown...
+                            echo '<option value="' . $email . '">' . $email . '</option>';
+                        }                 
+                        echo '</select>';
+                    }
+                    //Encerra a conexão.
+                    $conn->close();
+                ?>
+                <br><br>
+                <textarea name="areamessage" id="areamessage" cols="30" rows="10"></textarea>
+                <button onclick="redirect()" id="Respondido">Respondido</button>
+            </div>
         </div>
         <div class="menu2"><a href="rh.html">Recursos Humanos</a></div>
         <div class="menu3"><a href="desenvolvimento.html">Desenvolvimento</a></div>
@@ -61,18 +54,21 @@
         <div class="menu6"><a href="sair.html">Sair</a></div>
     </section>
     <script>
-        document.getElementById('messages').addEventListener('change', function() {
+            //Ouve o evento de mudança 'change' do dropdown...
+            document.getElementById('messages').addEventListener('change', function() {
+            //Recebe o email selecionado no dropdown
             var selectedEmail = this.value;
+            //Recebe a mensagem referente ao email do array
             var messages = <?php echo json_encode($contatos_emails_mensagens); ?>;
+            //A mensagem é então exibida na Textarea.
             document.getElementById('areamessage').value = messages[selectedEmail];
         });
     </script>
     <script>
+        //Função para redirecionar para outra página.
         function redirect() {
-
             var selectedEmail = document.getElementById('messages').value;
             window.location.href = 'respondido.php?email=' + selectedEmail;
-
         }
     </script>
 </body>
