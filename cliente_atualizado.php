2c0f75c4f3012035f7f7326213346ef9bf5c935d
Rafael Albuquerque <139004366+R4F43LL-D3VB3GINN3R@users.noreply.github.com>
2024-04-10 00:52:27 +0100

    Update cliente_atualizado.php

--- cliente_atualizado.php
+++ cliente_atualizado.php
@@ -9,72 +9,65 @@
 </head>
 <body>
 
-<?php
-
-$hostname = "localhost";
-$database = "db_devsync";
-$username = "root";
-$password = "";
-
-$conn = new mysqli($hostname, $username, $password, $database);
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    $cliente = $_POST['cliente'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $aplicacao = $_POST['aplicacao'];
-    $id = $_POST['id'];
-    $estado = $_POST['estado'];
-    $mensagem = $_POST['obs'];
-    $orcamento = $_POST['orcamento'];
-
-    $stmt = $conn->prepare("UPDATE tab_pedidos_aceitos 
-                            SET cliente = ?, email = ?, phone = ?, aplicacao = ?, orcamento = ?, observacoes = ?, estado = ?, atualizacao = NOW()
-                            WHERE id = ?");
-    $stmt->bind_param('ssssdssi', $cliente, $email, $phone, $aplicacao, $orcamento, $mensagem, $estado, $id);
-    $stmt->execute();
-
-    if ($stmt->affected_rows > 0) {
-
+    <?php
+    //Conecta à base de dados
+    include 'conexao.php';
+    //Se o método for post...
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        //Recupera dados do formulário.
+        $cliente = $_POST['cliente'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $aplicacao = $_POST['aplicacao'];
+        $id = $_POST['id'];
+        $estado = $_POST['estado'];
+        $mensagem = $_POST['obs'];
+        $orcamento = $_POST['orcamento'];
+        //Executa a Query SQL.
+        $stmt = $conn->prepare("UPDATE tab_pedidos_aceitos 
+                                SET cliente = ?, email = ?, phone = ?, aplicacao = ?, orcamento = ?, observacoes = ?, estado = ?, atualizacao = NOW()
+                                WHERE id = ?");
+        $stmt->bind_param('ssssdssi', $cliente, $email, $phone, $aplicacao, $orcamento, $mensagem, $estado, $id);
+        $stmt->execute();
+        //Se alguma linha for afetada...
+        if ($stmt->affected_rows > 0) {
     ?>
-
-          <div id="message_div">
+        <?php // Exibe a caixa de mensagem.?>
+        <div id="message_div">
             <h1>Cliente Atualizado</h1>
             <button onclick="redirect1()" id="buttonmessage">Ok</button>
-          </div>  
-          <script>
+        </div>  
+        <script>
+            //Função para redirecionar.
             function redirect1() {
-
                 window.location.href = "cliente_comercial.php";
-
             }
-          </script>
+        </script>
 
-    <?php
+        <?php
+        // Do contrário...
+        } else {
 
-    } else {
+        ?>
 
-    ?>
+        <?php // Exibe a caixa de mensagem.?>
         <div id="message_div">
             <h1>Não foi possível adicionar o Administrador</h1>
             <button onclick="redirect2()">Ok</button>        
         </div>
         <script>
+            //Função para redirecionar.
             function redirect2() {
-
                 window.location.href = "cliente_comercial.php";
-
             }
         </script>
-    <?php
 
+        <?php
+        }
+        //Encerra a conexão
+        $conn->close();
     }
-
-    $conn->close();
-
-}
-
-?>
+    ?>
+    
 </body>
 </html>
