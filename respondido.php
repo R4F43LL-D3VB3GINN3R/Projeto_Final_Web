47437d242adf1ecb1c906727adeede74f37036e6
Rafael Albuquerque <139004366+R4F43LL-D3VB3GINN3R@users.noreply.github.com>
2024-04-09 02:40:09 +0100

    Update respondido.php

--- respondido.php
+++ respondido.php
@@ -1,19 +1,14 @@
 <?php
-
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$database = "db_devsync";
-
-$conn = new mysqli($hostname, $username, $password, $database);
-
-$email = $_GET['email'];
-
-$stmt = $conn->prepare("UPDATE tab_contato SET RESPOSTA = 'RESPONDIDO' WHERE EMAIL = ?");
-$stmt->bind_param('s', $email);
-$stmt->execute();
-
-$conn->close();
-header("Location: caixa_de_mensagens.php");
-
+    //Conexão com base de dados.
+    include 'conexao.php';
+    //Recebe o email com o método Get.
+    $email = $_GET['email'];
+    //Executa a Query.
+    $stmt = $conn->prepare("UPDATE tab_contato SET RESPOSTA = 'RESPONDIDO' WHERE EMAIL = ?");
+    $stmt->bind_param('s', $email);
+    $stmt->execute();
+    //Enccerra a conexão.
+    $conn->close();
+    //Redireciona de volta à mesma página.
+    header("Location: caixa_de_mensagens.php");
 ?>
