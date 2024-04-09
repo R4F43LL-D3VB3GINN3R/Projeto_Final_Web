aebb754dfd859986835c7d84d0184b90d09c2949
Rafael Albuquerque <139004366+R4F43LL-D3VB3GINN3R@users.noreply.github.com>
2024-04-09 17:55:42 +0100

    Update pedidos_aceitos.php

--- pedidos_aceitos.php
+++ pedidos_aceitos.php
@@ -1,33 +1,32 @@
 <?php
-
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$database = "db_devsync";
-
-$conn = new mysqli($hostname, $username, $password, $database);
-
+//Conexão com a base de dados.
+include 'conexao.php';
+//Se as variáveis foram declaradas e diferentes de nulo...
+//Usamos o método get para buscá-las.
 if(isset($_GET['cliente']) && isset($_GET['email']) && isset($_GET['phone']) && isset($_GET['aplicacao']) && isset($_GET['atualizacao'])) {
-
+    //Guarda as variáveis em variáveis PHP.
     $cliente = $_GET['cliente'];
     $email = $_GET['email'];
     $phone = $_GET['phone'];
     $aplicacao = $_GET['aplicacao'];
     $atualizacao = $_GET['atualizacao'];
-
+    //Executa a Query.
+    //Mode o pedido para a tabela de pedidos aceitos.
     $stmt = $conn->prepare("INSERT INTO tab_pedidos_aceitos (cliente, email, phone, aplicacao, atualizacao) VALUES (?, ?, ?, ?, ?)");
     $stmt->bind_param('sssss', $cliente, $email, $phone, $aplicacao, $atualizacao);
     $stmt->execute();
-
+    //Executa a Query.
+    //Remove o pedido da tabela de pedidos.
     $stmt = $conn->prepare("DELETE FROM tab_pedido WHERE CLIENTE = ?");
     $stmt->bind_param('s', $cliente);
     $stmt->execute();
-
+    //Encerra a conexão.
     $conn->close();
+    //Redireciona para o url.
     header("Location: pedidos.php");
-
-} else {
-    echo "Parâmetros ausentes.<br>";
-}
-
+    //Do contrário...
+    } else {
+        //Exibe a mensagem.
+        echo "Parâmetros ausentes.<br>";
+    }
 ?>
