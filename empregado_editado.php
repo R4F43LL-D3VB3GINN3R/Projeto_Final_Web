879775a741a949ac7067fe04b27f470c942aa240
Rafael Albuquerque <139004366+R4F43LL-D3VB3GINN3R@users.noreply.github.com>
2024-04-10 02:03:17 +0100

    Update empregado_editado.php

--- empregado_editado.php
+++ empregado_editado.php
@@ -9,44 +9,40 @@
 </head>
 <body>
 
-<?php
+    <?php
 
-$hostname = "localhost";
-$database = "db_devsync";
-$username = "root";
-$password = "";
-
-$conn = new mysqli($hostname, $username, $password, $database);
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    $nif = $_POST['nif'];
-    $nome = $_POST['nome'];
-    $apelido = $_POST['apelido'];
-    $nome_completo = $_POST['nome_completo'];
-    $data_nascimento = $_POST['data_nascimento'];
-    $sexo = $_POST['sexo'];
-    $telefone = $_POST['telefone'];
-    $email = $_POST['email'];
-    $endereco = $_POST['endereco'];
-    $estado_civil = $_POST['estado_civil'];
-    $dependentes = $_POST['dependentes'];
-    $departamento = $_POST['departamento'];
-    $cargo = $_POST['cargo'];
-    $contrato = $_POST['contrato'];
-    $salario = $_POST['salario'];
-    $data_contratacao = $_POST['data_contratacao'];
-    $estado = $_POST['estado'];
-
-    $stmt = $conn->prepare("UPDATE tab_funcionario
-                            SET nif = ?, nome = ?, apelido = ?, nome_completo = ?, data_nascimento = ?, sexo = ?, telefone = ?, email = ?, endereco = ?, estado_civil = ?, dependentes = ?, departamento = ?, cargo = ?, tipo_contrato = ?, salario = ?, data_contratacao = ?, estado = ?, atualizacao = NOW()
-                            WHERE nome_completo = ?");
-    $stmt->bind_param('ssssssssssssssisss', $nif, $nome, $apelido, $nome_completo, $data_nascimento, $sexo, $telefone, $email, $endereco, $estado_civil, $dependentes, $departamento, $cargo, $contrato, $salario, $data_contratacao, $estado, $nome_completo);
-    $stmt->execute();
-
-    if ($stmt->affected_rows > 0) {
-
-    ?>
+    //Conexão com Base de Dados.
+    include 'conexao.php';
+    //Se o método for post.
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        //Recupera os dados enviados do formulário.
+        $nif = $_POST['nif'];
+        $nome = $_POST['nome'];
+        $apelido = $_POST['apelido'];
+        $nome_completo = $_POST['nome_completo'];
+        $data_nascimento = $_POST['data_nascimento'];
+        $sexo = $_POST['sexo'];
+        $telefone = $_POST['telefone'];
+        $email = $_POST['email'];
+        $endereco = $_POST['endereco'];
+        $estado_civil = $_POST['estado_civil'];
+        $dependentes = $_POST['dependentes'];
+        $departamento = $_POST['departamento'];
+        $cargo = $_POST['cargo'];
+        $contrato = $_POST['contrato'];
+        $salario = $_POST['salario'];
+        $data_contratacao = $_POST['data_contratacao'];
+        $estado = $_POST['estado'];
+        //Executa a Quer SQL.
+        //Atualiza a tabela de funcionários.
+        $stmt = $conn->prepare("UPDATE tab_funcionario
+                                SET nif = ?, nome = ?, apelido = ?, nome_completo = ?, data_nascimento = ?, sexo = ?, telefone = ?, email = ?, endereco = ?, estado_civil = ?, dependentes = ?, departamento = ?, cargo = ?, tipo_contrato = ?, salario = ?, data_contratacao = ?, estado = ?, atualizacao = NOW()
+                                WHERE nome_completo = ?");
+        $stmt->bind_param('ssssssssssssssisss', $nif, $nome, $apelido, $nome_completo, $data_nascimento, $sexo, $telefone, $email, $endereco, $estado_civil, $dependentes, $departamento, $cargo, $contrato, $salario, $data_contratacao, $estado, $nome_completo);
+        $stmt->execute();
+        //Se o número de linhas em result for maior do que zero...
+        if ($stmt->affected_rows > 0) {
+        ?>
 
           <div id="message_div">
             <h1>Dados Alterados com Sucesso</h1>
@@ -54,36 +50,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           </div>  
           <script>
             function redirect1() {
-
                 window.location.href = "cadastro_funcionarios.php";
-
             }
           </script>
 
     <?php
 
-    } else {
+        } else {
 
-    ?>
-        <div id="message_div">
-            <h1>Não há autorização para estas Alterações</h1>
-            <button onclick="redirect2()">Ok</button>        
-        </div>
-        <script>
-            function redirect2() {
-
-                window.location.href = "editar_empregado.php";
-
-            }
-        </script>
-    <?php
-
+        ?>
+            <div id="message_div">
+                <h1>Não há autorização para estas Alterações</h1>
+                <button onclick="redirect2()">Ok</button>        
+            </div>
+            <script>
+                function redirect2() {
+                    window.location.href = "editar_empregado.php";
+                }
+            </script>
+        <?php
+        }
+        //Encerra a conexão
+        $conn->close();
     }
 
-    $conn->close();
-
-}
-
-?>
+    ?>
 </body>
 </html>
